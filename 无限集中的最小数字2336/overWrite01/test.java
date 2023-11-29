2b77aadf9e7fb8917a30a3f95d83ec0b7754263d
只会CV <528375822@qq.com>
2023-11-29 09:16:55 +0800

    无限...

--- 无限集中的最小数字2336/overWrite01/test.java
+++ 无限集中的最小数字2336/overWrite01/test.java
@@ -1,19 +1,23 @@
 package title.无限集中的最小数字2336.overWrite01;
 
+import java.util.PriorityQueue;
+
 public class test {
 }
 
 class SmallestInfiniteSet {
-
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
     public SmallestInfiniteSet() {
-
+        for (int i = 1; i <= 1000; i++) {
+            pq.offer(i);
+        }
     }
 
     public int popSmallest() {
-
+        return pq.poll();
     }
 
     public void addBack(int num) {
-
+        if (!pq.contains(num)) pq.offer(num);
     }
 }
