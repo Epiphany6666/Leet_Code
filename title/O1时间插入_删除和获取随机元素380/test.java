fb099bc7a8bb7a7ff8c51611a2c47968410ca4e6
只会CV <528375822@qq.com>
2023-12-04 09:05:29 +0800

    if (loc != idx) map.put(nums[idx], loc);         nums[loc] = nums[idx--]; 被这句话给绕到了

---
+++ title/O1时间插入_删除和获取随机元素380/test.java
@@ -0,0 +1,38 @@
+package title.O1时间插入_删除和获取随机元素380;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class test {
+}
+
+class RandomizedSet {
+    Map<Integer, Integer> map = new HashMap<>();
+    static int[] nums = new int[2 * (int)1e5 + 10];
+    int idx = -1;
+    Random random = new Random();
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+        map.put(val, ++idx);
+        nums[idx] = val;
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+        int loc = map.remove(val);
+        //note 不能写反
+        //mind 被这句话给绕到了。。。
+//        if (loc != idx) nums[loc] = nums[idx--];
+//        map.put(nums[loc], loc);
+        if (loc != idx) map.put(nums[idx], loc);
+        nums[loc] = nums[idx--];
+        return true;
+    }
+
+    public int getRandom() {
+        return nums[random.nextInt(idx + 1)];
+    }
+}
